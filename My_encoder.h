516e33469454621f591fe7bf4483f1476fbc96ad
Bluedyss <63529028+Bluedyss@users.noreply.github.com>
2025-02-23 06:55:32 +0100

    Update My_encoder.h

--- My_encoder.h
+++ My_encoder.h
@@ -5,30 +5,36 @@
 
 class My_encoder {
   public:
-    // Konstruktor třídy
-    My_encoder(int pinA, int pinB);
+    // Constructor
+    My_encoder(int pinA, int pinB, int pinButton);
 
-    // Metody pro nastavení a získání hodnot
+    // Methods to initialize and get values
     void begin();
     int getPosition();
     void resetPosition();
+    bool isButtonPressed();
 
   private:
-    // Pin pro enkodér
+    // Pins for the encoder and button
     int _pinA;
     int _pinB;
+    int _pinButton;
     
-    // Aktuální pozice enkodéru
+    // Current position of the encoder
     volatile int _position;
     
-    // Poslední stav pinů
+    // Last state of the pins
     volatile int _lastStateA;
     volatile int _lastStateB;
     
-    // Metoda pro zpracování změny stavu enkodéru
+    // Button state
+    bool _lastButtonState;
+    bool _buttonPressed;
+    
+    // Method to handle encoder state change
     static void _isr();
 
-    // Statická proměnná pro zajištění správné obsluhy přerušení
+    // Static instance for proper interrupt handling
     static My_encoder* _instance;
 };
 
