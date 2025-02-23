bcf4b96be8cfdfa59a95b2ae6b553f389d28ebb0
Bluedyss <63529028+Bluedyss@users.noreply.github.com>
2025-02-23 06:54:48 +0100

    Update My_encoder.cpp

--- My_encoder.cpp
+++ My_encoder.cpp
@@ -1,41 +1,52 @@
-#ifndef MY_ENCODER_H
-#define MY_ENCODER_H
+#include "My_encoder.h"
 
-#include <Arduino.h>
+My_encoder* My_encoder::_instance = nullptr;
 
-class My_encoder {
-  public:
-    // Konstruktor třídy
-    My_encoder(int pinA, int pinB, int pinButton);
+// Konstruktor
+My_encoder::My_encoder(int pinA, int pinB) {
+  _pinA = pinA;
+  _pinB = pinB;
+  _position = 0;
+  _lastStateA = 0;
+  _lastStateB = 0;
+  
+  _instance = this;
+}
 
-    // Metody pro nastavení a získání hodnot
-    void begin();
-    int getPosition();
-    void resetPosition();
-    bool isButtonPressed();
+// Inicializace pinů a přerušení
+void My_encoder::begin() {
+  pinMode(_pinA, INPUT);
+  pinMode(_pinB, INPUT);
+  
+  // Připojení přerušení na pinA pro sledování změn
+  attachInterrupt(digitalPinToInterrupt(_pinA), _isr, CHANGE);
+}
 
-  private:
-    // Pin pro enkodér a tlačítko
-    int _pinA;
-    int _pinB;
-    int _pinButton;
-    
-    // Aktuální pozice enkodéru
-    volatile int _position;
-    
-    // Poslední stav pinů
-    volatile int _lastStateA;
-    volatile int _lastStateB;
-    
-    // Stav tlačítka
-    bool _lastButtonState;
-    bool _buttonPressed;
-    
-    // Metoda pro zpracování změny stavu enkodéru
-    static void _isr();
+// Funkce pro získání aktuální pozice enkodéru
+int My_encoder::getPosition() {
+  return _position;
+}
 
-    // Statická proměnná pro zajištění správné obsluhy přerušení
-    static My_encoder* _instance;
-};
+// Funkce pro resetování pozice enkodéru na nulu
+void My_encoder::resetPosition() {
+  _position = 0;
+}
 
-#endif
+// ISR pro zpracování změny stavu enkodéru
+void My_encoder::_isr() {
+  int stateA = digitalRead(_instance->_pinA);
+  int stateB = digitalRead(_instance->_pinB);
+
+  // Zjištění směru otáčení
+  if (stateA != _instance->_lastStateA) {
+    if (stateB != stateA) {
+      _instance->_position++;
+    } else {
+      _instance->_position--;
+    }
+  }
+
+  _instance->_lastStateA = stateA;
+  _instance->_lastStateB = stateB;
+}
+
