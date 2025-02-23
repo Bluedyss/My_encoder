7a1665540853363dc05d88d34e932eb89b92271e
Bluedyss <63529028+Bluedyss@users.noreply.github.com>
2025-02-23 06:55:52 +0100

    Update My_encoder.cpp

--- My_encoder.cpp
+++ My_encoder.cpp
@@ -2,42 +2,61 @@
 
 My_encoder* My_encoder::_instance = nullptr;
 
-// Konstruktor
-My_encoder::My_encoder(int pinA, int pinB) {
+// Constructor
+My_encoder::My_encoder(int pinA, int pinB, int pinButton) {
   _pinA = pinA;
   _pinB = pinB;
+  _pinButton = pinButton;
   _position = 0;
   _lastStateA = 0;
   _lastStateB = 0;
-  
+  _lastButtonState = HIGH; // Assuming the button is normally open
+  _buttonPressed = false;
+
   _instance = this;
 }
 
-// Inicializace pinů a přerušení
+// Initialization of pins and interrupts
 void My_encoder::begin() {
   pinMode(_pinA, INPUT);
   pinMode(_pinB, INPUT);
+  pinMode(_pinButton, INPUT_PULLUP); // The button uses the internal pull-up resistor
   
-  // Připojení přerušení na pinA pro sledování změn
+  // Attach interrupt to pinA for state change monitoring
   attachInterrupt(digitalPinToInterrupt(_pinA), _isr, CHANGE);
 }
 
-// Funkce pro získání aktuální pozice enkodéru
+// Function to get the current position of the encoder
 int My_encoder::getPosition() {
   return _position;
 }
 
-// Funkce pro resetování pozice enkodéru na nulu
+// Function to reset the encoder's position to zero
 void My_encoder::resetPosition() {
   _position = 0;
 }
 
-// ISR pro zpracování změny stavu enkodéru
+// Function to check if the button was pressed
+bool My_encoder::isButtonPressed() {
+  bool currentButtonState = digitalRead(_pinButton);
+  
+  // Check for button press (transition)
+  if (_lastButtonState == HIGH && currentButtonState == LOW) {
+    _buttonPressed = true;
+  } else {
+    _buttonPressed = false;
+  }
+
+  _lastButtonState = currentButtonState;
+  return _buttonPressed;
+}
+
+// ISR to handle encoder state change
 void My_encoder::_isr() {
   int stateA = digitalRead(_instance->_pinA);
   int stateB = digitalRead(_instance->_pinB);
 
-  // Zjištění směru otáčení
+  // Determine the direction of rotation
   if (stateA != _instance->_lastStateA) {
     if (stateB != stateA) {
       _instance->_position++;
@@ -49,4 +68,3 @@ void My_encoder::_isr() {
   _instance->_lastStateA = stateA;
   _instance->_lastStateB = stateB;
 }
-
